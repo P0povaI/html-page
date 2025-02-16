9313eacda934a6401f9c6e4c0539f7be11c3c3ee
Stoyan Stoyanov <14061867+stoyan-stoyanov@users.noreply.github.com>
2025-02-16 12:36:52 -0800

    Implemented a casino room and a boss fight

--- classes.py
+++ classes.py
@@ -2,46 +2,65 @@ import random
 
 
 class Player:
-    def __init__(self, name, player_class, damage,level):
+    def __init__(self, name, player_class, damage):
         self.name = name
         self.player_class = player_class
+        self.max_health = 100
         self.health = 100
         self.experience = 0
         self.damage = damage
         self.coins = 10
-        self.level=level
+        self.level=0
         self.kills=0
     
     def attack(self):
         return self.damage
 
+    def add_experience(self, exp):
+        self.experience+=exp
+        if self.experience > (self.level+1)*100:
+            print("You levelled up!")
+            self.level+=1
+            self.damage+=5
+            self.health+=10
+
 class Enemy:
-    def __init__(self, enemy_class, damage, health, coins):
+    def __init__(self, enemy_class):
         self.enemy_class=enemy_class
-        self.damage=damage
-        self.health=health
-        self.experience=2*self.health
-        self.coins=coins
+        self.damage=random.randint(10, 20)
+        self.health=random.randint(20,80)
+        self.experience=0.5*self.health
+        self.coins=random.randint(0, 3)
 
     def attack(self):
-        return self.damage    
+        return self.damage
+
+class Boss:
+    def __init__(self, enemy_class):
+        self.enemy_class=enemy_class
+        self.damage=random.randint(30, 60)
+        self.health=random.randint(60, 120)
+        self.experience=100
+        self.coins=random.randint(10, 30)
+
+    def attack(self):
+        return self.damage
 
 class RoomGenerator:
     def __init__(self):
-        self.room_types=["Bedroom","Chest","Fight","Shop"]
-        self.weights=[0.1,0.2,0.6,0.1]
+        self.room_types=["Bedroom", "Chest", "Fight", "Shop", "Casino", "Story"]
+        self.weights=[0.1, 0.1, 0.5, 0.1, 0.1, 0.1]
 
     def get_room(self):
         return random.choices(self.room_types,self.weights)[0]
     
 class ChestGenerator:
     def __init__(self):
-        self.loot_types=["Health potion","Scroll","Nothing","Explotion"]
+        self.loot_types=["Health potion", "Scroll", "Nothing", "Explosion"]
         self.weights=[0.2,0.2,0.4,0.2]
     
     def get_loot(self):
         return random.choices(self.loot_types,self.weights)[0]
-
 
 #change name to HitOrMiss
 class StrategicRetreatGenerator:
@@ -51,3 +70,6 @@ class StrategicRetreatGenerator:
 
     def get_action(self):
         return random.choices(self.action_types,self.weights)[0]
+
+class Storyline:
+    def __init__(self):
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import random, math
-from classes import Player, Enemy, RoomGenerator, ChestGenerator, StrategicRetreatGenerator
+from classes import Player, Enemy, RoomGenerator, ChestGenerator, StrategicRetreatGenerator, Boss
 
 
 if __name__ == "__main__":
-    p1 = Player(name="Ivan", player_class="Mage", damage=50, level=0)
+    p1 = Player(name="Veti", player_class="Mage", damage=20)
     print(p1.name)
     print(p1.player_class)
     print(p1.health)
@@ -17,17 +17,44 @@ if __name__ == "__main__":
         current_room=room_generator.get_room()
         current_chest=chest_generator.get_loot()
         current_action=strategic_retreat_generator.get_action()
+        if current_room=="Casino":
+            print("----- Casino -----")
+            print("You enter a casino!")
+            if p1.coins<1:
+                print("You don't have enough money to bet! Move to the next room.")
+            else:
+                print("You can choose to bet some money.")
+                print("You can either double or lose the money you bet.")
+                print("Enter the amount of money you want to bet")
+                bet=int(input())
+                while bet>p1.coins:
+                    print(f"You only have {p1.coins} coins. Enter a smaller amount.")
+                    bet=int(input())
+                print(f"You decided to bet {bet} coins.")
+                outcomes=["Win", "Lose"]
+                outcome_weights=[0.5, 0.5]
+                outcome=random.choices(outcomes, outcome_weights)[0]
+                if outcome=="Win":
+                    p1.coins+=bet
+                    print(f"Congratulations! You won {bet} coins!")
+                else:
+                    p1.coins-=bet
+                    print(f"You are out of luck. You lost {bet} coins!")
+
+            print(f"Health: {p1.health} Experience: {p1.experience} Player coins: {p1.coins}")
+            print("-------------------\n")
+
         if current_room=="Bedroom":
             print("----- Bedroom -----")
             print("You enter a bedroom!")
             if p1.coins<5:
                 print("You don't have enough money to sleep! Move to the next room.")
             else:
-                print("Do you want to take nap for 5 coins or countinue. Type N to take a nap or type C to continue.")
+                print("Do you want to take nap for 5 coins or countinue. Type n to take a nap or type c to continue.")
                 nap=input()
-                if nap=="N":
+                if nap=="n":
                     p1.coins-=5
-                    p1.health = 100
+                    p1.health = p1.max_health
                     print("You sleep and feel refreshed.")
 
             print(f"Health: {p1.health} Experience: {p1.experience} Player coins: {p1.coins}")
@@ -39,18 +66,24 @@ if __name__ == "__main__":
             if choice=="o":
                 print("You opened it")
                 if current_chest=="Health potion":
+                    last_health=p1.health
                     p1.health+=20
-                    if p1.health>=100:
-                        p1.health=100
-                    print(f"You found a health potion. 20 health added.")
+                    if p1.health>=p1.max_health:
+                        p1.health=p1.max_health
+                    print(f"You found a health potion. {p1.health-last_health} health added.")
                 elif current_chest=="Scroll":
-                    p1.experience+=20
+                    p1.add_experience(20)
                     print(f"You found a scroll. 20 exp added.")
                 elif current_chest=="Nothing":
                     print("You found nothing. Better luck next time!")
                 elif current_chest=="Explosion":
                     p1.health-=20
-                    print("The chest exploded. You lost 20 hp.")
+                    if p1.health<=0:
+                        print("You died!")
+                        print(f"Player level:  Player kills: {p1.kills}")
+                        exit()
+                    else:
+                        print("The chest exploded. You lost 20 hp.")
             else:
                 print("You moved to the next room!")
                 
@@ -59,9 +92,15 @@ if __name__ == "__main__":
             print("-------------------\n")
         elif current_room=="Fight":
             print("----- Fight -----")
-            print("You entered a room and there is an enemy that attacks you!")
-            damage=random.randint(10,20)
-            enemy=Enemy(enemy_class="Skeleton",damage=damage,health=random.randint(20,80),coins=random.randint(0,3))
+            enemies = ["enemy", "boss"]
+            chances = [0.8, 0.2]
+            enemy_type = random.choices(enemies, chances)[0]
+            if enemy_type == "enemy":
+                print("You entered a room and there is an enemy that attacks you!")
+                enemy=Enemy(enemy_class="Skeleton")
+            else:
+                print("You entered a room and there is a mighty enemy waiting for you!")
+                enemy=Boss(enemy_class="Dragon")
             print("You can choose fight or flight. Make your choice now f to fight and r to run away")
             action=input()
             if action=="f":
@@ -78,10 +117,10 @@ if __name__ == "__main__":
                         if p1.health<=0:
                             print("You died!")
                             print(f"Player level:  Player kills: {p1.kills}")
-                            break
+                            exit()
                     else:
                         print("You killed the enemy!")
-                        p1.experience+=enemy.experience
+                        p1.add_experience(enemy.experience)
                         if enemy.coins>0:
                             p1.coins+=enemy.coins
                             print(f"You received: {enemy.coins} coins")
@@ -90,7 +129,8 @@ if __name__ == "__main__":
                         
             elif action=="r":
                 print("You choose to run away. >_<")
-                p1.experience-=math.floor(p1.experience*0.20)
+                print("You lost all your experience after the last time you levelled up.")
+                p1.experience=p1.level*100
                 if current_action=="hit":
                     print("Unlucky you got hit. You shoud run faster.")
                     print(f"The enemy deals {enemy.damage/2}")
