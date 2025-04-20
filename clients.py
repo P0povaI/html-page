6b4a553e203121f04f9195ec86a519215842eace
Ivet Popova <ivetpopova@Ivets-Laptop.local>
2025-04-20 22:13:39 +0300

    .

--- clients.py
+++ clients.py
@@ -1,48 +1,28 @@
 import os
 from prompts import rpg_system_prompt
 from openai import OpenAI
+from pydantic import BaseModel
 
 client = OpenAI(
     # This is the default and can be omitted
     api_key=os.environ.get("OPENAI_API_KEY")
 )
 
+
+class StoryModel(BaseModel):
+    class ChapterModel(BaseModel):
+        room_description: str
+        chapter_story: str
+    chapters: list[ChapterModel]
+
 def generate_story():
-    response = client.responses.create(
+    response = client.beta.chat.completions.parse(
         model="gpt-4o",
-        input=[{"role": "system", "content": rpg_system_prompt},
+        messages=[{"role": "system", "content": rpg_system_prompt},
         {"role": "user", "content": "Generate a great engaging five chapter story."}
    ],
-    text={
-    "format": {
-        "type": "json_schema",
-        "name": "game story chapters",
-        "description": "Chapters 1 to 5 for the dungeon game",
-        "strict": True,
-        "schema": {
-            "type": "array",
-                    "description": "Array of five chapters",
-                    "items": {
-                        "type": "object",
-                        "properties": {
-                            "room_description": {
-                                "type": "string",
-                                "description": "A short description of the room that the character enters"
-                            },
-                            "chapter_story": {
-                                "type": "string",
-                                "description": "The story for this particular chapter"
-                            }
-                        },
-                        "additionalProperties": False,
-                        "required": ["room_description", "chapter_story"]
-                    }
-            },
-            "required": ["room_description", "chapter_story"],
-            "additionalProperties": False
-        }
-    }
+    response_format=StoryModel()
 
     )
 
-    return response.output_text
+    return response.choices[0].message.parsed
